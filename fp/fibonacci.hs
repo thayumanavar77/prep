c43e05c0bf912d72ae80fd0db9d72d2219eb6f63
root <root@ubuntu.(none)>
2013-12-27 00:55:49 -0800

    fp

--- fp/fibonacci.hs
+++ fp/fibonacci.hs
@@ -1,5 +1,9 @@
 import Control.Monad
 import Data.Array
+buildFibArray = array (0, 10000) [(i, fib i) | i <- [0..10000]]
+fib 0 = 0
+fib 1 = 1
+fib n = (buildFibArray ! (n-1)) + (buildFibArray ! (n-2))
 fibo n = farr ! n where
          farr = array (0, n) [(i, fib i) | i <- [0..n]]
          fib 0 = 0
@@ -9,5 +13,4 @@ main = do
   n <- readLn :: IO Int
   forM_ [1..n] $ const $ do
     fn <- readLn :: IO Int
-    putStrLn $ show $ fibo fn `mod` (10^8+7)
-
+    putStrLn $ show $ (buildFibArray ! fn) `mod` (10^8+7)
